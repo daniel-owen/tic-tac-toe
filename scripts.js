a574911f3782ae21c7cb5715e03daa720c78c3f6
daniel owen <dowen35@gmail.com>
2016-07-22 10:45:26 -0400

    2nd commit

--- scripts.js
+++ scripts.js
@@ -1,9 +1,35 @@
 // N grid version
 var whosTurn = 1; // start off on player 1's turn
 
+var alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
 var winners = [];
+var gridSize = 5;
+
+// a0, a1, a2, a3, a4...aN
+// b0, b1, b2, b3, b4...bN
+
+var diag1 = [];
+var diag2 = [];
 
 // 1. build a winners array
+for (var i = 0; i < gridSize; i++){
+	diag1.push(alph[i] + (gridSize-i)); // diag1
+	diag2.push(alph[i] + i); // diag 2
+	var winnersInsideH = [];
+	var winnersInsideV = [];
+	for (var j = 0; j < gridSize; j++){
+		// vertical winners
+		winnersInsideV.push(alph[j] + i);
+		// horizontal winners
+		winnersInsideH.push(alph[i] + j);
+	}
+	winners.push(winnersInsideV);
+	winners.push(winnersInsideH);
+}
+winners.push(diag1);
+winners.push(diag2);
+
+
 // 2. we need to populate the board
 
 var player1 = []; // array where we will stash the squares player1 has checked
